d0b0b1615fd196047c63bac6f12382fb782ed92b
Paul <paulfirmin1@gmail.com>
2020-11-05 18:53:44 +0000

    add task message removed on submit

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,6 @@
 import { App } from './app';
 import { parse, formatDistanceToNow } from 'date-fns';
+import Slideout from 'slideout';
 
 const Doc = (function () {
   // Tasks DOM
@@ -7,13 +8,18 @@ const Doc = (function () {
   const taskModal = document.getElementById('task-modal');
   const taskForm = document.getElementById('task-form');
   document.getElementById('due-time').defaultValue = '18:00';
+  document.getElementById('due-date').defaultValue = new Date().toISOString().substr(0, 10);
   // Projects DOM
   const projectList = document.getElementById('project-list');
   const projModal = document.getElementById('project-modal');
   const projForm = document.getElementById('project-form');
 
   const closeBtn = document.querySelectorAll('.close-btn');
-  const projMenu = document.getElementById('project-menu')
+  const projMenu = document.getElementById('project-menu');
+
+  document.querySelector('#menu-btn').addEventListener('click', function () {
+    projMenu.classList.toggle('show-menu');
+  });
 
   closeBtn.forEach((btn) => {
     btn.addEventListener('click', () => {
@@ -27,15 +33,15 @@ const Doc = (function () {
   });
 
   document.getElementById('filter-done').addEventListener('click', () => {
-    App.toggleFilter('done')
-    displayTasks()
-    document.getElementById('doneTick').classList.toggle('hide')
+    App.toggleFilter('done');
+    displayTasks();
+    document.getElementById('doneTick').classList.toggle('hide');
   });
 
   document.getElementById('filter-urgent').addEventListener('click', () => {
-    App.toggleFilter('urgent')
-    displayTasks()
-    document.getElementById('urgentTick').classList.toggle('hide')
+    App.toggleFilter('urgent');
+    displayTasks();
+    document.getElementById('urgentTick').classList.toggle('hide');
 
   });
 
@@ -44,18 +50,18 @@ const Doc = (function () {
   });
 
   document.getElementById('sort-due').addEventListener('click', () => {
-    App.dueDateSort()
-    displayTasks()
+    App.dueDateSort();
+    displayTasks();
   });
 
   document.getElementById('sort-added').addEventListener('click', () => {
-    App.addedDateSort()
-    displayTasks()
+    App.addedDateSort();
+    displayTasks();
   });
 
   window.onclick = function (event) {
-    let sortContent = document.getElementById('sortContent')
-    let filterContent = document.getElementById('filterContent')
+    let sortContent = document.getElementById('sortContent');
+    let filterContent = document.getElementById('filterContent');
 
     if (!event.target.matches('.btn')) {
       if (sortContent.classList.contains('show')) {
@@ -68,14 +74,11 @@ const Doc = (function () {
 
     if (event.target.matches('.project-list li')) {
       if (projMenu.classList.contains('show-menu')) {
-        projMenu.classList.remove('show-menu')
+        projMenu.classList.remove('show-menu');
       }
     }
   }
 
-  document.getElementById('menu-btn').addEventListener('click', () => {
-    projMenu.classList.toggle('show-menu');
-  })
 
   document.getElementById('new-task-btn').addEventListener('click', () => {
     taskModal.style.height = '100%';
@@ -127,69 +130,95 @@ const Doc = (function () {
     projectList.appendChild(li);
   }
 
+
   function appendTask(task) {
-    let dueDate
-    try {
-      dueDate = 'Due ' + formatDistanceToNow(
-        parse(task.dueDate, 'yyyy-MM-dd HH:mm', new Date()), { addSuffix: true })
-        + ` (${task.dueDate})`;
-    } catch {
-      dueDate = 'No deadline specified'
-    }
     const taskCard = document.createElement('div');
-    const delBtn = document.createElement('div');
-    const taskNotes = document.createElement('div')
-    const doneBtn = document.createElement('div')
-    const log = document.createElement('div')
-    log.textContent = task.log
-    log.classList.add('log', 'hide')
-    taskNotes.innerHTML = `<p class="task-notes">${task.notes}</p>`
-    taskNotes.setAttribute('class', 'hide')
+    const doneBtn = getDoneBtn(task, taskCard);
     taskCard.setAttribute('class', 'task-card');
+    taskCard.classList.add(getPriorityStyle(task));
+    taskCard.innerHTML = `
+        <h3 class="task-title">${task.title}</h3>
+        <div class="task-content"><span class="task-due">
+        <time>${getDueDate(task.dueDate)}</time></span>
+        `;
+    if (task.done) {
+      taskCard.classList.add('task-card-done');
+      doneBtn.classList.add('task-done');
+    }
+    const taskNotes = getTaskNotes(task)
+    taskCard.appendChild(doneBtn);
+    taskCard.appendChild(getDelBtn(task));
+    taskCard.appendChild(taskNotes);
+    taskCard.appendChild(getLog(task));
+    taskCard.addEventListener('click', () => {
+      taskNotes.classList.toggle('hide');
+    });
+    if (App.getActiveProject().items.length === 1) {
+      itemBoard.textContent = '';
+    }
+    itemBoard.appendChild(taskCard); 
+  }
+
+  function getTaskNotes(task) {
+    let taskNotes = document.createElement('div');
+    taskNotes.innerHTML = `<p class="task-notes">${task.notes}</p>`;
+    taskNotes.setAttribute('class', 'hide');
+    return taskNotes;
+  }
+
+  function getDelBtn(task) {
+    const delBtn = document.createElement('div');
     delBtn.setAttribute('class', 'del-btn');
     delBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
     delBtn.addEventListener('click', () => {
       App.deleteTask(task);
       displayTasks();
     });
-    doneBtn.setAttribute('class', 'done-btn')
-    doneBtn.innerHTML = '<i class="far fa-check-circle"></i>'
+    return delBtn;
+  }
+
+  function getDoneBtn(task, taskCard) {
+    let doneBtn = document.createElement('div');
+    doneBtn.setAttribute('class', 'done-btn');
+    doneBtn.innerHTML = '<i class="far fa-check-circle"></i>';
     doneBtn.addEventListener('click', () => {
-      App.toggleDone(task)
-      doneBtn.classList.toggle('task-done')
-      taskCard.classList.toggle('task-card-done')
-      if (App.getFilters().done == true) {
-        taskCard.classList.toggle('hide')
+      App.toggleDone(task);
+      doneBtn.classList.toggle('task-done');
+      taskCard.classList.toggle('task-card-done');
+      if (App.getFilters().done) {
+        taskCard.classList.toggle('hide');
       }
     })
+    return doneBtn
+  }
 
-    taskCard.innerHTML = `
-        <h3 class="task-title">${task.title}</h3>
-        <div class="task-content"><span class="task-due">
-        <time>${dueDate}</time></span>
-        `;
+  function getLog(task) {
+    const log = document.createElement('div');
+    log.textContent = task.log;
+    log.classList.add('log', 'hide');
+    return log;
+  }
 
-    if (task.priority == 'high') {
-      taskCard.classList.add('urgent');
-    } else if (task.priority == 'med') {
-      taskCard.classList.add('medium-pri');
-    } else if (task.priority == 'low') {
-      taskCard.classList.add('low-pri');
+  function getDueDate(date) {
+    let dueDate;
+    try {
+      dueDate = 'Due ' + formatDistanceToNow(
+        parse(date, 'yyyy-MM-dd HH:mm', new Date()), { addSuffix: true })
+        + ` (${date})`;
+    } catch {
+      dueDate = 'No deadline specified';
     }
+    return dueDate; 
+  }
 
-    if (task.done === true) {
-      taskCard.classList.add('task-card-done')
-      doneBtn.classList.add('task-done')
+  function getPriorityStyle(task) {
+    if (task.priority === 'high') {
+      return 'urgent';
+    } else if (task.priority === 'med') {
+      return 'medium-pri';
+    } else if (task.priority === 'low') {
+      return 'low-pri';
     }
-    taskCard.appendChild(doneBtn)
-    taskCard.appendChild(delBtn);
-    taskCard.appendChild(taskNotes);
-    taskCard.appendChild(log)
-
-    itemBoard.appendChild(taskCard);
-    taskCard.addEventListener('click', () => {
-      taskNotes.classList.toggle('hide')
-    });
   }
 
   function displayProjectList() {
@@ -204,9 +233,9 @@ const Doc = (function () {
       App.getActiveProject().title;
     itemBoard.textContent = '';
 
-    if (taskList.length == 0) {
+    if (taskList.length === 0) {
       itemBoard.textContent =
-        "Press the '+' button to add a task to this project."
+        'Press the "+" button to add a task to this project.';
     } else {
       for (const i of taskList) {
         appendTask(i);
